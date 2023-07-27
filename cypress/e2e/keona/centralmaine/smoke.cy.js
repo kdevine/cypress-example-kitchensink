2ee8d9733ce45193cd5137de7a73942aa00b8783
Kevin Devine <kevin@keonahealth.com>
2023-07-27 16:12:49 -0400

    take out only

--- cypress/e2e/keona/centralmaine/smoke.cy.js
+++ cypress/e2e/keona/centralmaine/smoke.cy.js
@@ -28,7 +28,7 @@ describe('Central Maine Tests', () => {
     });
     cy.visit(locations[0].url,{timeout:600000});
   });
-  it.only(locations[0].client + ' ' + locations[0].area + ' triage test', function() {
+  it(locations[0].client + ' ' + locations[0].area + ' triage test', function() {
     cy.get('#btnProviderLogin').click();
     cy.get('#MainContent_liLogin_txtUserName').type(this.data.nurseLogin);
     cy.get('#MainContent_liLogin_txtPassword').type(this.data.nursePassword);
